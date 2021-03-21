1f8d911c1304a1322811187fad6927d114c5c14f
Sayansree <sayansreeparia@gmail.com>
2021-03-22 03:35:59 +0530

    example
    Signed-off-by: Sayansree <sayansreeparia@gmail.com>

--- example.cpp
+++ example.cpp
@@ -4,24 +4,32 @@ using namespace std;
 int main(){
 
 	Serial *s=new Serial();
-	// vector<std::string> ports=s->scanPorts();
-	// for(std::string s:ports)
-	// 	cout<<s<<endl;
-	// std::string responce = s->autoConnect("Quantum Ground Station",B2000000);
+
+    //example to scan all Serial ports on linux machine
+	vector<std::string> ports=s->scanPorts();
+	for(std::string s:ports)
+		cout<<s<<endl;
+	
+
+    //example to open a definite Serial port and perform handshake
 	if(s->begin("/dev/ttyUSB0",B2000000)){
 		cout<<"\n  ttyUSB0 Opened Successfully\n";
         if(!s->handshake("Quantum Ground Station"))
 			cout<<"\n No Device detected\n";
 	    else
             cout<<"\n"<<" handshake successful\n";
-            vector<std::string> ports=s->scanPorts();
-            for(std::string s:ports)
-                cout<<s<<endl;
+        s->end();
     }else
 		cout<<"\n  Error! in Opening ttyUSB0\n";
-	
-	std::string responce = s->autoConnect("Quantum Ground Station",B2000000);
+
+
+
+	//Search and connect to any device with the given handshake string at given 
+	std::string responce = s->autoConnect("handshake text",B2000000);
 	cout<<"\n  ttyUSB0 Closed\n";
 	s->end();
+
+
+    
 	return 0;
 }
